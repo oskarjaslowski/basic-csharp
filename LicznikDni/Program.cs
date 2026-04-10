5ee7999dd17269b1c5485e5634f674bf83a10ac5
Oskar Jasłowski <oskarjaslowski77@gmail.com>
2026-04-10 22:29:35 +0200

    Add files via upload

---
+++ LicznikDni/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace LicznikDni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double waga = UzyskajDane("Podaj wagę w kilogramach (kg): ");
+                double wysokosc = UzyskajDane("Podaj wzrost w centymetrach (cm): ");
+
+                double bmi = ObliczBMI(waga, wysokosc);
+
+                Console.WriteLine($"Twoje BMI wynosi: {bmi:F2}");
+
+                string kategoria = OcenStan(bmi);
+                Console.WriteLine($"Kategoria BMI: {kategoria}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Podano niepoprawne dane. Wprowadź liczbę.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static double UzyskajDane(string dana)
+        {
+            Console.Write(dana);
+            double danaPrzekazana;
+            while (!double.TryParse(Console.ReadLine(), out danaPrzekazana))
+            {
+                Console.WriteLine("Niepoprawne dane. Wprowadź liczbę.");
+                Console.Write(dana);
+            }
+            return danaPrzekazana;
+        }
+
+        static double ObliczBMI(double waga, double wysokosc)
+        {
+            double wysokoscMetry = wysokosc / 100;
+            return waga / (wysokoscMetry * wysokoscMetry);
+        }
+
+        static string OcenStan(double bmi)
+        {
+            if (bmi < 18.5)
+                return "Niedowaga";
+            else if (bmi >= 18.5 && bmi <= 24.9)
+                return "Waga normalna";
+            else if (bmi >= 25 && bmi <= 29.9)
+                return "Nadwaga";
+            else
+                return "Otyłość";
+        }
+    }
+}
---
+++ Petla/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        int[] tablica = new int[100];
+
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            tablica[i] = random.Next(1, 1000);
+        }
+
+        Console.WriteLine("Zawartość tablicy:\n");
+
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i] + "\t");
+
+            if ((i + 1) % 10 == 0)
+            {
+                Console.Write("\n\n");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
